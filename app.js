4d4d4f8d78878d55907a39e2c52786d64bce4fc7
leandroluz97 <leandrodaluz97@gmail.com>
2022-11-13 18:58:15 +0000

    added base pug file

--- app.js
+++ app.js
@@ -78,7 +78,10 @@ app.use((req, res, next) => {
 //ROUTES
 
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The hiker track',
+    user: 'Leandro',
+  });
 });
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
