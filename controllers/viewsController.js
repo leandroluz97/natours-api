290af95d6a4b6ac4082921729afc817a45e23814
leandroluz97 <leandrodaluz97@gmail.com>
2022-11-16 23:06:02 +0000

    added tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,13 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res) => {
+  const { slug } = req.params;
+  const tour = await Tour.findOne({ slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+  res.status(200).render('tour', {
+    tour,
+  });
+});
