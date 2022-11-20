c961900278e315fe57c68f7ca494590f50526042
leandroluz97 <leandrodaluz97@gmail.com>
2022-11-20 23:27:39 +0000

    added tour title

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -20,6 +20,7 @@ exports.getTour = catchAsync(async (req, res) => {
     fields: 'review rating user',
   });
   res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
     tour,
   });
 });
