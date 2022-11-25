52bed043a91ebe1c05e828c1a75fe8d7bbedeea0
leandroluz97 <leandrodaluz97@gmail.com>
2022-11-25 23:57:33 +0000

    added connection to mapbox

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -3,4 +3,8 @@
 console.log('hello from the client side');
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
-console.log(locations);
+mapboxgl.accessToken = process.env.MAPBOX_TOKEN;
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+});
