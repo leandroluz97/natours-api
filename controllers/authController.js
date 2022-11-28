e2f454f93b0b263b33c31f80fccdd470f326a8ed
leandroluz97 <leandrodaluz97@gmail.com>
2022-11-28 21:35:11 +0000

    fixed  req.locals  to res.locals

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,8 +28,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     expiresIn: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    secure: process.env.NODE_ENV.trim() === 'production', //send just for HTTPS
-    httpOnly: true, // cookie can't be accessed by js
+    // secure: process.env.NODE_ENV.trim() === 'production', //send just for HTTPS
+    httpOnly: false, // cookie can't be accessed by js
   };
 
   res.cookie('jwt', token, cookieOptions);
@@ -67,6 +67,16 @@ exports.login = catchAsync(async (req, res, next) => {
 
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expiresIn: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    // secure: process.env.NODE_ENV.trim() === 'production', //send just for HTTPS
+    httpOnly: false, // cookie can't be accessed by js
+  };
+
+  res.cookie('jwt', token, cookieOptions);
+
   res.status(201).json({
     status: 'success',
     token,
@@ -84,6 +94,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
 
   if (!token) {
@@ -113,6 +125,35 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   //GRANT ACCESS TO PROTECTED ROUTE
   req.user = currentUser;
+  next();
+});
+
+//ONLY FOR RENDERED PAGES
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check if its there
+  let token;
+  if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+
+    if (!currentUser) {
+      return next();
+    }
+
+    // 4) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+
+    //THE IS A LOGGED IN USER
+    req.loacals.user = currentUser;
+    next();
+  }
   next();
 });
 
---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+/*eslint-disable */
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+
+  login({ email, password })
+    .then(console.log)
+    .catch((error) => console.log(error.response.data));
+});
+
+async function login({ email, password }) {
+  if (!email || !password)
+    throw new Error('You should provied a email or password.');
+  if (email.trim() === '' || password.trim() === '')
+    throw new Error('Your email or password should not be empty.');
+
+  const response = await axios.post(
+    'http://localhost:3000/api/v1/users/login',
+    {
+      email,
+      password,
+    }
+  );
+  return response;
+}
