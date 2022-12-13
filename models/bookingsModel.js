8d3ef99e54563cde18b0dc61db52b9e5b4fa89ea
leandroluz97 <leandrodaluz97@gmail.com>
2022-12-13 23:08:57 +0000

    added booking schema

---
+++ models/bookingsModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = mongoose.Schema(
+  {
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Booking must belong to a Tour!'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Booking must belong to a User!'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    createAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    paid: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+bookingSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'user',
+  }).populate({
+    path: 'user',
+    select: 'name',
+  });
+  next();
+});
+
+const Booking = mongoose.model('Review', bookingSchema);
+
+module.exports = Booking;
